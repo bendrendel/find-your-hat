73c80f4bf98360a599c580defb6d5d1d2659bad4
bendrendel <bfdrendel@gmail.com>
2021-06-13 21:54:07 -0600

    Set up Field class

--- main.js
+++ main.js
@@ -4,3 +4,30 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+let direction = prompt('Which way?')
+
+class Field {
+    constructor(field) {
+        this.field = field;
+    }
+
+    print() {
+        this.field.forEach(row => {
+            let printRow = row.join('');
+            console.log(printRow);
+        })
+    }
+
+    
+}
+
+testField = [
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+];
+
+myField = new Field(testField);
+
+myField.print();
